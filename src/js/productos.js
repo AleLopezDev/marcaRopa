2e15e267e0d59911c9a0fc57dcfb648a043c76ea
Ale <alejandrolopezaguilar.dev@gmail.com>
2023-07-22 15:29:30 +0200

    Commit inicial

---
+++ src/js/productos.js
@@ -0,0 +1,32 @@
+export const productos = [
+  {
+    nombre: "El Campero",
+    img: "https://continuousbrand.com/wp-content/uploads/2023/06/El-Campero.jpg",
+    precio: 30 + "€",
+  },
+  {
+    nombre: "Continuousgueta Pink",
+    img: "https://continuousbrand.com/wp-content/uploads/2023/06/Continuousgueta-Pink.jpg",
+    precio: 30 + "€",
+  },
+  {
+    nombre: "Continuousgueta Blue",
+    img: "https://continuousbrand.com/wp-content/uploads/2023/06/Continuousgueta-Blue.jpg",
+    precio: 30 + "€",
+  },
+  {
+    nombre: "Sunny Vibes",
+    img: "https://continuousbrand.com/wp-content/uploads/2023/06/Sunny-vibes.jpg",
+    precio: 30 + "€",
+  },
+  {
+    nombre: "Gorra rosa",
+    img: "https://continuousbrand.com/wp-content/uploads/2023/06/02_Gorra-rosa-45-derecha.png",
+    precio: 30 + "€",
+  },
+  {
+    nombre: "Gorra azul",
+    img: "https://continuousbrand.com/wp-content/uploads/2023/06/02_Gorra-azul-45-izquierda.png",
+    precio: 30 + "€",
+  },
+];
